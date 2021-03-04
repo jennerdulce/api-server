14ca079b97d7c39951403b9176b376bd8884bebe
Jenner Dulce <jennerdulce@gmail.com>
2021-03-03 19:46:40 -0800

    added

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
 
 require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 const server = require('./src/server.js');
 const PORT = process.env.PORT || 3000;
 
-const MONGODB_URL = 'mongodb://localhost:27017/lab4DB';
+const MONGODB_URL = 'mongodb+srv://jennercf:1234@cluster0.nx7ji.mongodb.net/Cluster0?retryWrites=true&w=majority';
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
 
 mongoose.connect(MONGODB_URL, options)
